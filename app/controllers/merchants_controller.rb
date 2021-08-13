0fd74630276bf1766b4aeffd80a3f1f633c730f2
InSunc <insunck@gmail.com>
2021-08-13 20:11:04 +0300

    Create API endpoint for uploading csv

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,6 +1,26 @@
+require 'rack/multipart'
+
 class MerchantsController < ApplicationController
-  before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
+  # before_action :authenticate_user!
+
+  def create
+    env = {
+      'CONTENT_TYPE' => request.headers['Content-Type'],
+      'CONTENT_LENGTH' => request.body.length,
+      'rack.input' => StringIO.new(request.body.read)
+    }
+
+    data = Rack::Multipart.parse_multipart(env)
+
+    render json: read_parsed_multipart(data)
+  end
 
   def index
   end
+
+  private
+    def read_parsed_multipart(data)
+      data['file'][:tempfile].read
+    end
 end
